04b8d31134b528b6479bff496c4cd21be8b1708f
Ben Coveney <bencoveney@gmail.com>
2018-05-06 01:34:34 +0100

    local database dev

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  ssl: !!parseInt(process.env.DATABASE_SSL)
 });
 
 express()
